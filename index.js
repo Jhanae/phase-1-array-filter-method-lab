75a77f0c75e37bcd40de45135d76301066b0094c
Jhanae <jhanaechisholm12@gmail.com>
2021-05-19 10:31:59 -0400

    commit tree 92b4334daf44084b08f6af17359b0c462b1ce0b6

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Code your solution here
+function findMatching(drivers, name)
+{
+    const newCollection = [];
+
+    for(const user of drivers){
+        
+        if(user.toLowerCase() == name.toLowerCase() )
+        newCollection.push(user);
+    }
+    return newCollection
+}
+
+function fuzzyMatch(drivers, letter)
+{
+    const newCollection = [];
+
+    for(const user of drivers){
+        const begin = user.slice(0, letter.length)
+        if(begin.toLowerCase() == letter.toLowerCase())
+        newCollection.push(user);
+        
+    }
+    return newCollection
+}
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function matchName(drivers, name)
+{
+    for (var i=0; i < drivers.length; i++){
+       if(drivers[i].name == name )
+       {
+        return drivers.filter( user => user.name === name)
+       }
+
+}
+}
